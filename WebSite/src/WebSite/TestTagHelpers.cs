d7ebec3d37d448d0a38dac6824c7656e87bdda93
mperdeck <mperdeck@gmail.com>
2015-12-11 21:15:53 +1100

    Added JSNLog special tag

---
+++ WebSite/src/WebSite/TestTagHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Razor.TagHelpers;
+
+namespace Website.TagHelpers
+{
+    [HtmlTargetElement("jl-javascript-logging-configuration-code2", Attributes = RequestIdAttributeName)]
+    public class TestTagHelpers : TagHelper
+    {
+        private const string RequestIdAttributeName = "requestid";
+
+        [HtmlAttributeName(RequestIdAttributeName)]
+        public string RequestId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+
+            if (output == null)
+            {
+                throw new ArgumentNullException("output");
+            }
+
+            output.Content.SetContent("xxxxxxxxxxxxxxx");
+        }
+    }
+}
