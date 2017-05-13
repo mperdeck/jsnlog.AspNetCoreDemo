e341c9df5469c6bbb5f0a4f0ca13d31f2c7a973b
mperdeck <matt.perdeck@readify.net>
2017-05-13 11:30:54 +1000

    added test code, logging via console.log for now

--- WebSite/WebSite/wwwroot/js/site.js
+++ WebSite/WebSite/wwwroot/js/site.js
@@ -1,1 +1,17 @@
-﻿// Write your Javascript code.
+﻿
+function GenerateException() {
+    // Try to access variable that doesn't exist
+    var x = xyz;
+}
+
+function LogMessage() {
+    var message = document.getElementById('message').value;
+
+    if (document.getElementById('trace').checked) { console.log("trace " + message);}
+    if (document.getElementById('debug').checked) { console.log("debug " + message); }
+    if (document.getElementById('info').checked) { console.log("info " + message); }
+    if (document.getElementById('warn').checked) { console.log("warn " + message); }
+    if (document.getElementById('error').checked) { console.log("error " + message); }
+    if (document.getElementById('fatal').checked) { console.log("fatal " + message); }
+}
+
